024d95d9c51085f69d552bdcee65ef50f2369185
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2023-02-10 18:05:34 -0500

    input and toggle logic

--- client/src/Login.js
+++ client/src/Login.js
@@ -9,6 +9,8 @@ function Login({setCurrentUser}) {
         username: "",
         password: ""
     })
+    const [showPassword, setShowPassword] = useState(false);
+    const [passwordButton, setPasswordButton] = useState("Show Password")
 
     //Login submission 
     // This code is a function that is called when a form is submitted. It creates an object with the username and password from the form, then sends a POST request to the '/login' endpoint with the user object as the body. If the response is successful, it will parse the response as JSON and set the current user.
@@ -17,13 +19,13 @@ function Login({setCurrentUser}) {
         e.preventDefault();
         const user = {
             username,
-            password
-        }
+            password}
 
         fetch('/login', {
             method: 'POST',
-            headers: { 'Accept': 'application/json',
-            'Content-Type' : 'application/json'},
+            headers: { 
+                'Accept': 'application/json',
+                'Content-Type' : 'application/json'},
             body: JSON.stringify(user)
         })
         .then(res => {
@@ -32,9 +34,22 @@ function Login({setCurrentUser}) {
                     setCurrentUser(user)
                 })
             }
+            else {
+                res.json().then(json => {alert(json.error)})
+            }
         })
     }
 
+
+    //Handle character input in form
+   // This code is a function that takes an event (e) as an argument. It uses the spread operator to create a new object based on the existing loginForm object, and then updates the value of the property that matches the name of the event target with the value of the event target.
+    const handleInput = (e) => {
+        setLoginForm({...loginForm, [e.target.name]: e.target.value})
+    }
+
+    const togglePassword = () => {
+        setShowPassword(!showPassword);
+    }
 
   return (
     <div>
@@ -47,13 +62,14 @@ function Login({setCurrentUser}) {
                 for="exampleEmail"
                 hidden
                 >
-                Email
+                Username
                 </Label>
                 <Input
-                id="exampleEmail"
-                name="email"
-                placeholder="Email"
-                type="email"
+                onChange={handleInput}
+                id="username"
+                name="username"
+                placeholder="username"
+                type="name"
                 />
             </FormGroup>
             {' '}
@@ -65,15 +81,18 @@ function Login({setCurrentUser}) {
                 Password
                 </Label>
                 <Input
-                id="examplePassword"
+                id="password"
                 name="password"
                 placeholder="Password"
-                type="password"
+                type= {showPassword ? "text" : "password"}
                 />
             </FormGroup>
             {' '}
             <Button>
                 Submit
+            </Button>
+            <Button onClick={togglePassword}>
+               Toggle Password
             </Button>
         </Form>
     </div>
